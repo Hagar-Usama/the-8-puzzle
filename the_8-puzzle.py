6b8790cfaf5524b9042893a1775d6c1e61b09412
Hagar Usama <Hagar_Usama@outlook.com>
2020-04-30 18:39:46 +0200

    add class node skeleton

---
+++ the_8-puzzle.py
@@ -0,0 +1,27 @@
+
+class Node():
+    # state is list of list. Each list is a row
+    def __init__(self, state, parent):
+        self.state = state
+        self.parent = parent
+    
+    def dfs(self, goal):
+        pass
+
+    def bsf(self, goal):
+        pass
+
+    def A_start(self, goal, heuristic):
+        pass
+    
+    def print_state(state):
+        pass
+
+    def traverse_tree(state):
+        pass
+
+def parse_input(state):
+    pass
+
+def parse_output(state):
+    pass
