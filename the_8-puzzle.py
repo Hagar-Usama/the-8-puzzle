665f6264b590f5c8770c0626e692ee1893cb6510
Hagar Usama <Hagar_Usama@outlook.com>
2020-05-01 18:41:59 +0200

    * BFS and DFS done partially * Working A*

--- the_8-puzzle.py
+++ the_8-puzzle.py
@@ -30,11 +30,13 @@ def print_green(msg):
 
 class Node():
     # state is list of list. Each list is a row
-    def __init__(self, state, parent):
+    def __init__(self, state, parent, depth=0, hcost=0):
         self.state = state
         self.parent = parent
         self.neighbors = []
         self.tileRow ,self.tileCol = get_empty_tile(self)
+        self.hcost = hcost
+        self.depth = depth
         
     
     def dfs(self, goal):
@@ -61,13 +63,47 @@ class Node():
             return Failure
         '''
 
+        frontier = [self]
+        explored = []
+        
+        while frontier:
+            current_node = frontier.pop(-1)
+            if current_node.state not in explored:
+                print("adding current_node in explored")
+                explored.append(current_node.state)
+            #explored.add(current_node.state)
+
+            print_yellow(f'current state: {current_node.state}')
+
+            if current_node.state == goal:
+                print_blue("Target found")
+                return True
+            
+            status = self.try_move(current_node)
+            print_blue(status)
+
+            for i in range(0,len(status)):
+                if status[i]:
+                    new_state = self.move(i,current_node)
+                    print_red(f'move {new_state}')
+
+                    
+                    if new_state not in explored:
+                        print("state not in explored")
+                        ns = Node(new_state, current_node)
+                        frontier.append(ns)
+                        current_node.neighbors.append(ns) 
+
+            print(f'explored: {explored}')    
+
+
 
 
 
 
         pass
 
-    def bsf(self, goal):
+    def bfs(self, goal):
         '''
         dfs(initial_state, goal):
             # returns Success or Failure
@@ -147,7 +183,55 @@ class Node():
             
             return Failure
 
+
+            note: f(n) = g(n) + h(n)
+            h(n) = hueristic cost
+            g(n) = distance from current node to the root node
+            g(n) = depth
+            f(n) = total cost
+
         '''
+        # let the
+        frontier = [self]
+        explored = []
+
+        while frontier:
+            frontier = sorted(frontier, key= lambda c:c.cost)
+            current_node = frontier.pop(0)
+            if current_node.state not in explored:
+                print("adding current_node in explored")
+                explored.append(current_node.state)
+
+
+            print_yellow(f'current state: {current_node.state}')
+
+            if current_node.state == goal:
+                print_blue("Target found")
+                return True
+
+            status = self.try_move(current_node)
+            print_blue(status)
+
+            for i in range(0,len(status)):
+                if status[i]:
+                    depth = current_node.depth + 1
+
+                    h_cost = get_heuristic_val()
+
+                    new_state = self.move(i,current_node, depth=depth)
+                    print_red(f'move {new_state}')
+
+                    
+                    if new_state not in explored:
+                        print("state not in explored")
+                        ns = Node(new_state, current_node)
+                        frontier.append(ns)
+                        current_node.neighbors.append(ns) 
+
+            print(f'explored: {explored}')
+            
+            
+
 
         pass
     
@@ -244,6 +328,11 @@ class Node():
 
         return board
 
+    def get_heuristic_val(state, goal, option):
+        if option == 'Manhattan':
+            pass
+
+
 
     
 
@@ -272,7 +361,35 @@ class Node():
 
         
     
+def get_heuristic_val(state, goal_state, option='Manhattan' ):
+    h = []
+    row_mat = []
+    col_mat = []
 
+    EMPTY_TILE = 0
+    # Assumption: 8-puzzle has no duplicates ever
+    if option == 'Manhattan':
+        for i in state:
+            for j in i:
+                if j != EMPTY_TILE:
+                    row1,col1 =  get_tile_coodrinates(state, j)
+                    row2,col2 =  get_tile_coodrinates(goal_state, j)
+                    h_tile_cost = abs(row1 - row2) + abs(col1 - col2)
+                    #print_green(f'({row1-row2},{col1-col2})')
+                    h.append(h_tile_cost)
+        
+    elif option == 'Euclidean':
+         for i in state:
+            for j in i:
+                if j != EMPTY_TILE:
+                    row1,col1 =  get_tile_coodrinates(state, j)
+                    row2,col2 =  get_tile_coodrinates(goal_state, j)
+                    h_tile_cost = (row1 - row2)**2 + (col1 - col2)**2
+                    h.append(h_tile_cost)
+    
+    print_yellow(h)
+    return sum(h)
+        
 def move(movement,my_node):
         LEFT = 0
         RIGHT = 1
@@ -334,6 +451,22 @@ def get_empty_tile(my_node):
                     
         return -1,-1    
 
+def get_tile_coodrinates(state, tile_key):
+
+    ''' takes a state list and key and returns rol and col'''
+
+    for i, element in enumerate(state):
+        #print_yellow(i)
+        try:
+            j = element.index(tile_key)
+        except ValueError:
+            continue
+
+        return i,j
+                    
+    return -1,-1    
+
+
 def parse_input(state):
     pass
 
@@ -392,12 +525,34 @@ def find(c, board):
     
 
 def main():
+
+    state10 = [[5,0,8],[4,2,1], [7,3,6]]
+    state20 = [[1,2,3],[4,5,6], [7,8,0]]
+    get_heuristic_val(state10, state20)
+    get_heuristic_val(state10, state20, option='Euclidean')
+    print_blue('*.*.*.*.*')
+
     state1 = [[1,2,5],[3,4,0],[6,7,8]]
     state2 = [[0,1,2],[3,4,5],[6,7,8]]
 
     ln = Node(state1, None)
-    print_blue(ln.bsf(state2))
+    l2 =  Node(state1, None)
+    ln.cost = 15
+    l2.cost = 2
 
+    #print_blue(ln.bfs(state2))
+    frontier = [ln , l2]
+    frontier = sorted(frontier, key= lambda c:c.cost)
+    print(frontier[0].cost)
+    print(frontier[1].cost)
+
+    #l2.dfs(state2)
+    
+    ll =[1,2,8, 12,5]
+    print(ll.pop(0))
+    print(ll)
+    print(ll.pop(-1))
+    print(ll)
 
 if __name__ == "__main__":
     main()
